b2ba9971c1051ad45e6db1593791419d6303c908
Jayapal Reddy <saijoshna@Vasudeva.local>
2026-08-31 11:01:13 +0530

    fix: removed redundant reanimated and tamagui plugins causing iOS bundler crash

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config')
-const { withTamagui } = require('@tamagui/metro-plugin')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 })
 
-module.exports = withTamagui(config, {
-  components: ['tamagui'],
-  config: './tamagui.config.ts',
-  outputCSS: './tamagui-web.css',
-})
+module.exports = config
